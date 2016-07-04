e52ab69c6fe3bf81084e42d9ef16ee6468e9cfdb
aholovchak <andriy531@gmail.com>
2016-07-04 23:51:05 +0300

    added topseller items

--- src/app/Controllers/ProvenceController.js
+++ src/app/Controllers/ProvenceController.js
@@ -11,6 +11,30 @@
 
     /** @ngInject */
     function ProvenceController() {
-        //var vm = this;
+        var vm = this;
+
+        vm.topseller = [
+            {
+                id: 1,
+                name: "Quinta de Sao Vicente Essig",
+                price:  9.90,
+                inhalt: "0.25 Liter (39.60/ 1 Liter)",
+                img_url: "http://www.shopwaredemo.de/media/image/2d/a4/95/SW10101_200x200.png"
+            },
+            {
+                id: 2,
+                name: "Steiriches Kurbiskenol ggA",
+                price:  14.00,
+                inhalt: "0.25 Liter (56.00/ 1 Liter)",
+                img_url: "http://www.kfofinho.com/wp-content/uploads/2016/03/Balsamico-Essig.png"
+            },
+            {
+                id: 3,
+                name: "Quinta de Sao Vicente OI",
+                price:  6.90,
+                inhalt: "0.25 Liter (37.60/ 1 Liter)",
+                img_url: "http://www.shopwaredemo.co.uk/media/image/52/1c/6b/SW10102_600x600.png"
+            }
+        ]
     }
 })();
