a942f0df552b98fb08c2ed4f367ff074ac794f7d
aholovchak <andriy531@gmail.com>
2016-07-06 00:38:18 +0300

    added static pages

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -24,6 +24,18 @@
                 templateUrl: 'app/templates/provence.html',
                 controller: 'ProvenceController',
                 controllerAs: 'provence'
+            }).state('main.adventure', {
+                url: 'adventure',
+                templateUrl: 'app/templates/adventure.html',
+            }).state('main.tradition', {
+                url: 'tradition',
+                templateUrl: 'app/templates/tradition.html',
+            }).state('main.specials', {
+                url: 'specials',
+                templateUrl: 'app/templates/specials.html',
+            }).state('main.blog', {
+                url: 'blog',
+                templateUrl: 'app/templates/blog.html',
             });
 
         $urlRouterProvider.otherwise('/home');
