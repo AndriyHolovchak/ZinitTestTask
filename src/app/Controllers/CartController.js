467dc5f83e9e283e4ef7a839ab51ea2cc0cd1e53
aholovchak <andriy531@gmail.com>
2016-07-06 11:49:50 +0300

    added zur kasse

--- src/app/Controllers/CartController.js
+++ src/app/Controllers/CartController.js
@@ -6,16 +6,16 @@
     'use strict';
 
     angular
-            .module('zinitTestTask')
-            .controller('CartController', CartController);
+        .module('zinitTestTask')
+        .controller('CartController', CartController);
 
     /** @ngInject */
-    function CartController($rootScope, $localStorage, lodash, $state) {
+    function CartController($rootScope, $localStorage, lodash, $state, $http) {
         var vm = this;
         vm.products = [];
         vm.isCart = false;
 
-        if($localStorage.cart) {
+        if ($localStorage.cart) {
             vm.products = $localStorage.cart;
             if (vm.products.length) {
                 vm.isCart = true;
@@ -26,14 +26,29 @@
             lodash.remove($localStorage.cart, function(i) {
                 if (i.id == id) {
                     $rootScope.cartSum -= i.price;
-                    $rootScope.cartSum = Math.round($rootScope.cartSum*100)/100;
+                    $rootScope.cartSum = Math.round($rootScope.cartSum * 100) / 100;
                     return true;
                 }
             });
             if (!vm.products.length) {
                 vm.isCart = false;
             }
-            $state.go($state.current, {}, {reload: true});
+            $state.go($state.current, {}, { reload: true });
+        }
+
+        vm.zurKasse = function() {
+            var sendData = JSON.stringify(vm.products, null, 4);
+            $http({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/',
+                method: "POST",
+                data: sendData,
+                headers: { 'Content-Type': 'application/json' }
+            }).success(function() {
+                alert('Senden JSON Data: ' + sendData);
+            }).error(function() {
+                alert("error");
+            });
+        
         }
     }
 })();
